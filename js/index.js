4a7707323ceb71e05e99b299d992a6b8a7a69f90
Samy Ali Sanchez <102321253+SamyAliSanchez@users.noreply.github.com>
2022-07-26 11:07:08 +0200

    bonus 2 fixed

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
 let hacker1 = "Carol";
 console.log(`The driver's name is ${hacker1}`);
 // expected output: "The driver's name is Eli"
-
 let hacker2 = "Carlos";
 console.log(`The navigator's name is ${hacker2}`);
 // expected output: "The navigator's name is Javi"
@@ -11,9 +10,7 @@ console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
-  console.log(
-    `The driver has the longest name, it has ${hacker1.length} characters.`
-  );
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 } else if (hacker1.length < hacker2.length) {
   console.log(
     `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
@@ -29,7 +26,8 @@ if (hacker1.length > hacker2.length) {
 // Iteration 3: Loops
 // Count the number of words (option 2, in one line):
 // we convert the text into an array by identifying each blank space and then we get the array's length
-console.log(`Second word count is ${trimmedLorem.split(" ").length}.`);
+
+/* console.log(`Second word count is ${trimmedLorem.split(" ").length}.`); */
 
 // Iteration 3.1 'Name' to 'N A M E'
 let upperCaseName = "";
@@ -69,8 +67,9 @@ if (text.toLowerCase() === backwardsText.toLowerCase()) {
   console.log(`${text} is not a Palindrome`);
 }
 
+
 // Bonus 2: Lorem ipsum counter
-const loremIpsum =
+ const loremIpsum =
   "Convallis elit erat vestibulum urna diam potenti nostra sollicitudin. Nullam sed nibh. Velit proin id. Placerat magna arcu. Arcu nibh tempor. Posuere parturient aenean gravida erat et. Viverra ut vivamus. Fermentum neque placerat. Phasellus pellentesque gravida suscipit tempus mattis in pellentesque lectus. Nunc diam eu. Justo amet sed euismod pellentesque pellentesque. Erat tellus nonummy risus nibh vel. Sit aliquam sodales a turpis sit. Eu nec id vel dui fusce sit vestibulum duis cras id mauris. Id velit duis diam dignissim ac nec varius orci tortor taciti nisl leo sed ultrices. Sapien placerat id.";
 
 // first we trim the extra blank spaces
@@ -84,3 +83,29 @@ for (character of trimmedLorem) {
 }
 console.log(`Word count is ${wordCount + 1}.`);
 
+
+/* // Bonus 2: Lorem ipsum counter
+const lorem = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue condimentum porta. Donec sem tellus, tincidunt et est eu, fringilla vestibulum turpis. Fusce dapibus, leo et auctor lacinia, nunc lacus malesuada est, at interdum nibh leo sed leo. Fusce aliquam finibus tortor quis auctor. Proin maximus mi in dictum auctor. Cras fermentum nisi sed mi consequat, sit amet rhoncus magna tristique. Mauris aliquam tincidunt nunc hendrerit malesuada. Praesent eget nisl augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam lorem ligula, malesuada eleifend quam sed, molestie dapibus massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus velit lectus, ullamcorper pharetra nisl nec, tincidunt hendrerit urna. Sed id ullamcorper leo, ut vulputate quam. Sed eu arcu at elit rhoncus lacinia. Sed aliquam libero sed turpis pulvinar, sed blandit nisi mollis. Mauris felis nulla, tempus sit amet ante eu, condimentum vestibulum justo.
+
+Maecenas non nisi viverra, semper justo a, hendrerit ligula. Fusce interdum metus elit, ut imperdiet turpis posuere eu. Nullam dignissim sollicitudin libero, ut lacinia purus varius nec. Fusce interdum ex in bibendum imperdiet. Proin ac eros diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque dui purus, viverra quis scelerisque at, sodales vitae nisl. Sed scelerisque massa at lobortis pellentesque. Pellentesque in consequat diam, nec congue dolor. Proin ac est ultrices, ultricies lectus sit amet, pellentesque dolor. Maecenas vitae elit metus.
+
+Curabitur at ex eu augue tempor maximus. Fusce sodales diam ac leo volutpat, sodales molestie neque molestie. Praesent et pretium elit, accumsan sagittis sem. Proin euismod, enim vitae mollis cursus, ante magna iaculis ex, quis mollis ipsum sapien a nulla. Donec ultrices, risus vel ultricies vulputate, quam enim vestibulum massa, vitae euismod lorem lorem ac tortor. Etiam vulputate id sem eget pellentesque. Sed malesuada tristique quam ut auctor. Sed quam metus, maximus quis faucibus sit amet, semper ac purus. Quisque sed ipsum arcu. Vestibulum non urna pellentesque quam volutpat porta vel et nisl. Nam imperdiet auctor ipsum ac cursus. Integer vel nunc eget ligula porta dictum.
+`;
+
+
+let etCounter = 0;
+
+for (var i = 0; i < lorem.length; i++) {
+    if (lorem[i] === "e" && lorem[i+1] === "t") {
+        if (lorem[i-1] === " " && lorem[i+2] === " ") {
+            etCounter++;
+        }
+    }
+}
+
+console.log(`Lorem Ipsum contains ${etCounter} 'et' words.`); */
+ 
+
+
+ 
